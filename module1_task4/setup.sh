c7fb2fd1cb5c3f25040269e31d259588fabde4d7
Gabriel <fossecruor@gmail.com>
2023-05-03 04:32:07 -0700

    Tested in local banger

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,7 +1,7 @@
-apt-get update && apt-get install -y hugo make
-apt install git wget &> /dev/null
+apt-get update && apt-get install -y hugo make git wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_Linux-64bit.tar.gz
-rm /usr/bin/hugo && mv ./hugo /usr/bin/
+tar -xf hugo_0.111.3_Linux-64bit.tar.gz
+mv ./hugo /usr/bin/
 wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
