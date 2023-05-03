2a1cbab5affdbc23d63cf417f8a26a168e4b4a10
Gabriel <fossecruor@gmail.com>
2023-05-03 00:27:47 -0700

    remove old setup and redirect output to /dev/null

--- module1_task3/setup.sh
+++
@@ -1,3 +0,0 @@
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-apt-get update && apt-get install -y hugo make
-exit
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-apt-get update && apt-get install -y hugo make && apt install git
+apt-get update && apt-get install -y hugo make git &> /dev/null
 git clone https://github.com/PhYdrogen/holbertonschool-validation
 cd holbertonschool-validation/
 make build
