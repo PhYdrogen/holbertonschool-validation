5ad4396bc3142bdf6043fd0d8c977590a478540a
Gabriel <fossecruor@gmail.com>
2023-05-03 00:24:18 -0700

    Trying setup file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y hugo make && apt install git
+git clone https://github.com/PhYdrogen/holbertonschool-validation
+cd holbertonschool-validation/
+make build
 exit
