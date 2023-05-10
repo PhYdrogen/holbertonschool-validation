c56e532f52b0ca8ffb6657712325811e95d11334
Gabriel <fossecruor@gmail.com>
2023-05-10 01:52:25 -0700

    get pranked

--- module2_task5/setup.sh
+++ module2_task5/setup.sh
@@ -6,3 +6,5 @@ wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 rm go1.20.4.linux-amd64.tar.gz && rm hugo_0.111.3_Linux-64bit.tar.gz
+npm install -g markdownlint-cli
+npm install -g markdown-link-check
