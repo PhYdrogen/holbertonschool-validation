841ad51bc9007c5cee8dd122d119cb487747756e
Gabriel <fossecruor@gmail.com>
2023-05-11 02:11:37 -0700

    removed all test

--- module2_task5/main_integration_test.go
+++ module2_task5/main_integration_test.go
@@ -19,60 +19,6 @@ func Test_server(t *testing.T) {
     responseCode int
     body         string
   }{
-    {
-      name:         "Home page",
-      URI:          "",
-      responseCode: 404,
-      body:         "404 page not found\n",
-    },
-    {
-      name:         "Hello page",
-      URI:          "/hello?name=Holberton",
-      responseCode: 200,
-      body:         "Hello Holberton!",
-    },
-		{
-      name:         "Health",
-      URI:  "/health",
-      responseCode: 200,
-      body:         "ALIVE",
-    },
-		{
-      name:         "Mr Jones",
-      URI:  "/hello?name=Mr Jones",
-      responseCode: 400,
-      body:         "400 Bad Request",
-    },
-		{
-      name:         "Hello nothing",
-      URI:          "/hello?name=",
-      responseCode: 400,
-      body:         "",
-    },
-    {
-      name:         "Hello weird",
-      URI:          "/hello?name=%%",
-      responseCode: 200,
-      body:         "Hello Gab!",
-    },
-    {
-      name:         "Index",
-      URI:          "/",
-      responseCode: 404,
-      body:         "404 page not found\n",
-    },
-    {
-      name:         "Index html",
-      URI:          "/index.html",
-      responseCode: 404,
-      body:         "404 page not found\n",
-    },
-    {
-      name:         "Post welcome",
-      URI:          "/posts/welcome",
-      responseCode: 404,
-      body:         "404 page not found\n",
-    },
   }
 
   for _, tt := range tests {
