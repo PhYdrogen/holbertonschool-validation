2a0c247f5bd026769c8e60e976996d769f23d36a
Gabriel <fossecruor@gmail.com>
2023-05-19 09:24:37 -0700

    added conditional in setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -3,5 +3,11 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_Li
 tar -xf hugo_0.111.3_Linux-64bit.tar.gz
 sudo mv ./hugo /usr/bin/
 sudo rm hugo_0.111.3_Linux-64bit.tar.gz
+
+if ! [ -x "$(command -v npm)" ]; then
+  echo 'Error: npm is not installed.' >&2
+  exit 1
+fi
+
 sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
