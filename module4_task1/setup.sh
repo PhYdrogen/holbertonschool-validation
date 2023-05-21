7f436309b4187fe6b74cd1eb6dd3ffa2ad182acd
Gabriel <fossecruor@gmail.com>
2023-05-21 04:37:49 -0700

    sudo for the install

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -1,1 +1,1 @@
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_linux-amd64.deb -o hugo.deb && apt install ./hugo.deb && rm hugo.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_linux-amd64.deb -o hugo.deb && sudo apt install ./hugo.deb && rm hugo.deb
